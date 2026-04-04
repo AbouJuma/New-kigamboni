641eae7903e6f5cff62b80219d062615cee3065b
Geeks <abubakari@webcorporation.co.tz>
2026-04-04 10:59:04 +0300

    changes

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,15 +3,19 @@ const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 mix.js('resources/src/main.js', 'public')
+  .sass('resources/src/assets/styles/sass/themes/lite-purple.scss', 'public/css', {
+    implementation: require('sass')
+  })
   .js('resources/src/login.js', 'public')
   .vue();
 
 mix.webpackConfig({
+  cache: false,
   output: {
     filename: 'js/[name].min.js',
     chunkFilename: 'js/bundle/[name].[hash].js',
   },
-  stats: 'errors-only', // Add this if you want minimal output
+  stats: 'errors-only',
   plugins: [
     new MomentLocalesPlugin(),
     new CleanWebpackPlugin({
